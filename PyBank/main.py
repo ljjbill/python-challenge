b606fddc00436844326b7734a2e0b7940e12ec0a
Bill Liu <ljjbill@gmail.com>
2018-11-04 14:16:57 -0600

    pybank completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,20 +10,27 @@ with open(pybankCsv, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    # Skip the header
     header = next(csvreader)
     
-
+    # Make a list of all rows in csv file after header
     rows=[]
+    # Start point to sum the total value
     netvalue=0
+    # Start point of previous value profit/loss
     preivousvalue=0
+    # Start point to sum the total monthly difference
     total_monthly_diff=0
-    Max_value=-99999
-    Min_value=99999
+    # Start point of max value of the list, make it small enough to make the sure first row value will replace this
+    Max_value=-99999999999
+    # Start point of min value of the list, make it big enough to make the sure first row value will replace this
+    Min_value=99999999999
     # Read through each row of data after the header
     for row in csvreader:
+        # add the value to list "rows"
         rows.append(row)
-        netvalue=netvalue+float(row[1])
+        # add the current value to netvalue
+        netvalue=netvalue+int(row[1])
         # The greatest increase/decrease in profits (date and amount) over the entire period
         if int(row[1])>Max_value:
             Max_month=row[0]
@@ -33,19 +40,21 @@ with open(pybankCsv, 'r') as csvfile:
             Min_value=int(row[1])
         
 
-
+        # The monthly difference is the current value - previous month value
         monthly_diff=float(row[1])-preivousvalue
+        # Sum of all the monthly differnce
         total_monthly_diff=total_monthly_diff+monthly_diff
+        # Replace the previous value with current value so it can be used for the calculation from the next row
         preivousvalue=float(row[1])
         
         
     
     # The total net amount of "Profit/Losses" over the entire period
-    total_net_amt=netvalue
+    total_net_amt=round(netvalue,0)
     # The total number of months included in the dataset
     total_num_month=len(rows)
-    # The average change in "Profit/Losses" between months over the entire period
-    avg_change=(total_monthly_diff-float(rows[0][1]))/(total_num_month-1)
+    # The average change in "Profit/Losses" between months over the entire period (When we calculate the monthly diff, the first row was deducted by 0 so we need to deduct the total by the first row value itself)
+    avg_change=round((total_monthly_diff-float(rows[0][1]))/(total_num_month-1),2)
     
     
     print("Financial Analysis")
@@ -56,4 +65,6 @@ with open(pybankCsv, 'r') as csvfile:
     print(f"Greatest Increase in Profits: {Max_month} (${Max_value})")
     print(f"Greatest Decrease in Profits: {Min_month} (${Min_value})")
 
-    
+    with open("Output.txt", "w") as text_file:
+        print(f"Financial Analysis\n----------------------------\nTotal Months: {total_num_month}\nTotal: ${total_net_amt}\nAverage  Change: ${avg_change}\nGreatest Increase in Profits: {Max_month} (${Max_value})\nGreatest Decrease in Profits: {Min_month} (${Min_value})", file=text_file)
+
